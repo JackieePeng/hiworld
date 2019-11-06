99b7c6f1f503ed5699aa4f559b3938b3da594dbe
Jackie <32533350+JackieePeng@users.noreply.github.com>
2019-11-06 09:43:44 +0800

    Update HiWorld.java

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,7 +1,7 @@
-import java.util.Date;
+
 
 public class HiWorld {
     public static void main(String args[]){
-        System.out.println(new Date());
+        System.out.println(new String("test"));
     }
 }
